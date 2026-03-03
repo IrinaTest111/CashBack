79ad121414c8346b0199191633500ff8b559fcfa
Art test <vis-buch@yandex.ru>
2026-03-03 18:42:07 +0300

    Adding TestNG tests

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+package ru.netology.service;
 
 public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
 }
